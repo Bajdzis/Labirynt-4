0254f283975b7358f154b0c4c59362e845ea976b
Bajdzis <rafal612b@wp.pl>
2024-05-28 19:00:39 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/game/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, './public/build'),
+    filename: "[name]-bundle.js" // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
