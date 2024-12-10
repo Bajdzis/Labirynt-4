ad0fa0fcfe7f679d38bbfaf329cdfd12eee3c93c
Bajdzis <rafal612b@wp.pl>
2024-12-10 07:45:31 +0100

    update build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
 /* eslint-disable */
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: {
-    main: "./src/game/index.ts",
-  },
-  output: {
-    path: path.resolve(__dirname, "./public/build"),
-    filename: "[name]-bundle.js", // <--- Will be compiled to this single file
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-      },
-    ],
-  },
+
+module.exports = (env, argv) => {
+
+  console.log("MODE:",argv.mode);
+
+  return {
+    mode: argv.mode,
+    devtool: argv.mode === 'development' ? "inline-source-map" : false,
+    entry: {
+      main: "./src/game/index.ts",
+    },
+    output: {
+      path: path.resolve(__dirname, "./public/build"),
+      filename: "[name]-bundle.js", // <--- Will be compiled to this single file
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+        },
+      ],
+    },
+  };
 };
